3500d08f35220aacd25382358aff40d07b291097
Vishal Narayan <nvnaraya@amazon.com>
2024-05-20 09:54:41 -0700

    hide sidebar with css.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import streamlit as st
+st.set_page_config(initial_sidebar_state="collapsed")
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
 st.write("This page is empty.")
 
