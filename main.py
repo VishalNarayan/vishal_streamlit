85445b85093ae79ea4e03fb36cf3a51d5551067d
Vishal Narayan <nvnaraya@amazon.com>
2024-05-27 10:35:36 -0700

    sidebar

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import streamlit as st
 st.set_page_config(initial_sidebar_state="collapsed")
 
-st.markdown(
-    """
-<style>
-    [data-testid="collapsedControl"] {
-        display: none
-    }
-</style>
-""",
-    unsafe_allow_html=True,
-)
+with st.sidebar:
+    st.write('hi')
+    pass
+
+# st.markdown(
+#     """
+# <style>
+#     [data-testid="collapsedControl"] {
+#         display: none
+#     }
+# </style>
+# """,
+#     unsafe_allow_html=True,
+# )
 
 st.write("This page is empty.")
 
